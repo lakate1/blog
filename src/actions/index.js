a4e6fb2604ed28e66241fb58c1421306a1d52c99
Leah Queen <leah836@gmail.com>
2020-10-12 17:18:32 -0600

    Added a post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,8 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-    const promise = jsonPlaceholder.get("/posts");
-    return {
-        type: "FETCH_POSTS",
-        payload: promise
-    };
-};
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response })
+   };
+
